d3562679a7b063a0478faeaad809d8e5320fcf44
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-03-27 15:07:09 -0300

    Add Menu Tabs with React Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import Home from './components/Home';
 import Alunos from './components/Alunos';
 import Cadastrar from './components/Cadastrar';
 import Sobre from './components/Sobre';
-import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Teste app react</h1>
       <BrowserRouter>
-      <ul>
-        <li><Link to="/">Página Inicial</Link></li>
-        <li><Link to="/alunos">Lista de alunos</Link></li>
-        <li><Link to="cadastrar">Cadastro de alunos</Link></li>
-        <li><Link to="/sobre">Sobre</Link></li>
-      </ul>
+
+      <Nav variant="tabs">
+          <Nav.Link as={Link} to="/">Página Inicial</Nav.Link>
+          <Nav.Link as={Link} to="/alunos">Lista de alunos</Nav.Link>
+          <Nav.Link as={Link} to="/cadastrar">Cadastro de alunos</Nav.Link>
+          <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+
+      </Nav>
 
       <Routes>
-        <Route path='/' index element={<Home/>}></Route>
+      <Route path='/' index element={<Home/>}></Route>
         <Route path='/alunos' element={<Alunos/>}></Route>
         <Route path='/cadastrar' element={<Cadastrar/>}></Route>
         <Route path='/sobre' element={<Sobre/>}></Route>
